1cb0b8c1034ecabc138c42f1379ad700c48f4f61
Andrew Feitosa <andrewcsz@gmail.com>
2020-08-08 11:53:36 -0300

    console

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
 Dino.on("ticket", function(data) {
+    console.log("[APP]", ticket)
     document.getElementById("ticket-subject").innerHTML = data.ticket.subject
     document.getElementById("ticket-id").innerHTML = data.ticket._id
 })
