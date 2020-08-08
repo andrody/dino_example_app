df705efe0b9db74c726d990d0c4d53722213c4d3
Andrew Feitosa <andrewcsz@gmail.com>
2020-08-08 12:02:51 -0300

    fix ticket-subject

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 Dino.on("ticket", function(data) {
-    console.log("[APP]", ticket)
+    console.log("[APP]", data)
+    console.log(document, document.getElementById("ticket-subject"))
     document.getElementById("ticket-subject").innerHTML = data.ticket.subject
     document.getElementById("ticket-id").innerHTML = data.ticket._id
 })
